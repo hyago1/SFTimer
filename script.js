adce819c630623dee3a8b8aa83e9e6e8e3060f51
hyago philip <hyagophilip@gmail.com>
2022-04-13 02:37:37 -0300

    added delete icon

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ function showTimes() {
     if (solves[i] != null) {
       numberSolves++;
       solve.innerHTML = numberSolves;
-      table.innerHTML += `<li id="${i}">${solves[i]}  <img class="iconDelete" onclick="deleteTime(${i})" src="./images/delete.svg" alt="" srcset=""></li>`;
+      table.innerHTML += `<li id="${i}">${solves[i]}  <img class="iconDelete" onclick="deleteTime(${i})" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABmJLR0QA/wD/AP+gvaeTAAAAo0lEQVRIieWTTQqDMBCFP6xb7yg9Qo/SHik3qUgXFlxWqGI3WYQ4Jc5I0NIHswnM+5lH4F9wAXpg9tP7NzVcQGIdFxIWFhcJzBk4D4Yb9vtfYzKpg2aDucWuJHDPLbAlwcJc9gQSTsAbfcEjUMZkUoIJeKi9Q+tFkgJg60Hc+SZg6UHc2S3BIQVUJ7J0oDJVAQPr/8DL76hwBp4ryDug1pL/Dj5MW2BhteGWuAAAAABJRU5ErkJggg=="/></li>`;
     }
   }
   document.getElementById("times").lastChild.scrollIntoView();
