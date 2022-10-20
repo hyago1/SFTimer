8d22fd4f218ede8eb0bba359352e114e2054a016
Hyago Philip <hyagophilip@gmail.com>
2022-10-20 10:32:42 -0300

    error removed

--- script.js
+++ script.js
@@ -80,7 +80,10 @@ function showTimes() {
       table.innerHTML += `<li id="${i}">${solves[i]}  <img class="iconDelete" onclick="deleteTime(${i})" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABmJLR0QA/wD/AP+gvaeTAAAAo0lEQVRIieWTTQqDMBCFP6xb7yg9Qo/SHik3qUgXFlxWqGI3WYQ4Jc5I0NIHswnM+5lH4F9wAXpg9tP7NzVcQGIdFxIWFhcJzBk4D4Yb9vtfYzKpg2aDucWuJHDPLbAlwcJc9gQSTsAbfcEjUMZkUoIJeKi9Q+tFkgJg60Hc+SZg6UHc2S3BIQVUJ7J0oDJVAQPr/8DL76hwBp4ryDug1pL/Dj5MW2BhteGWuAAAAABJRU5ErkJggg=="/></li>`;
     }
   }
-  document.getElementById("times").lastChild.scrollIntoView();
+  if (numberSolves != 0) {
+    document.getElementById("times").lastChild.scrollIntoView();
+  }
+  
 }
 
 function deleteTime(id) {
